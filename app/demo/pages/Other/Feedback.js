552f7ec5d750c504ef93203a9e283a361d22c491
xylxpm <bobo_xylxpm@163.com>
2017-07-25 13:45:31 +0800

    更新登录组件

--- app/demo/pages/Other/Feedback.js
+++ app/demo/pages/Other/Feedback.js
@@ -2,16 +2,27 @@
  * 列表  页面
  */
 
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 import {
     AppRegistry,
 
     StyleSheet,
     Text,
     View,
-    Button
+    Button,
+    ListView
 } from 'react-native';
+import RefreshableListView from 'react-native-refreshable-listview';
+import delay from 'react-native-refreshable-listview/lib/delay';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {connect} from 'react-redux';
+import * as USER from '../../actions/UserAction';
+
+var makeSequence = (n) => Array.apply(null, {length: n}).map((v, i) => i)
+
+var ds = new ListView.DataSource({
+    rowHasChanged: (r1, r2) => r1.text !== r2.text
+})
 
 
 class Feedback extends Component {
@@ -31,11 +42,37 @@ class Feedback extends Component {
 
     }
 
+    getInitialState() {
+        var rows = makeSequence(100).map((n) => ({text: 'not refreshed '+n}))
+        return {dataSource: ds.cloneWithRows(rows)}
+    }
+
+    reloadItems() {
+        return delay(1000).then(() => {
+            var rows = makeSequence(100).map((n) => ({text: 'refreshed '+n}))
+            this.setState({dataSource: ds.cloneWithRows(rows)})
+        })
+    }
+    renderItem(item) {
+        return (
+            <View style={{height: 40, backgroundColor: '#ffffff', borderWidth: 0.5, borderColor: '#d6d7da'}}>
+                <Text>
+                    {item.text}
+                </Text>
+            </View>
+        )
+    }
 
     render() {
         return (
-            <View>
-                <Text>喵列表</Text>
+            <View style={{marginTop: 20}}>
+                <RefreshableListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderItem.bind(this)}
+                    loadData={this.reloadItems.bind(this)}
+                    refreshDescription="Refreshing items"
+                    refreshPrompt="Pull down to refresh"
+                />
             </View>
         )
     }
@@ -47,4 +84,11 @@ const styles = StyleSheet.create(
 )
 
 
-export default Feedback;
+export default connect((state) => {
+    const {UserReducer} = state;
+    const routes = state.nav.routes;
+    return {
+        UserReducer,
+        routes
+    };
+}, {USER})(Feedback)
